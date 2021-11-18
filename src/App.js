969922d29a3d00b5760852f3a68f77d44c2f0afe
Steven Ross <ross.steven@me.com>
2021-11-18 10:33:42 -0500

    destructure components to children and pass props directly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import { SplitScreen } from './SplitScreen';
 
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green'}}>Left!</h1>
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'green'}}>{name}</h1>
 } 
 
-const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'red'}}>Right!</h1>
+const RightHandComponent = ({ message }) => {
+  return <h1 style={{ backgroundColor: 'red'}}>{message}</h1>
 } 
 
 function App() {
   return (
-    <SplitScreen 
-      left={LeftHandComponent}
-      right={RightHandComponent}
-    />
+    <SplitScreen leftWeight={1} rightWeight={3}
+    >
+      <LeftHandComponent name={"Steve"}/>
+      <RightHandComponent message={"Hello!"}/>
+    </SplitScreen>
   );
 }
 
