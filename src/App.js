0f92d08ad387dcba58e4c0b34c065556066f3ca2
Steven Ross <ross.steven@me.com>
2021-11-18 11:04:27 -0500

    list items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import './App.css';
-import { SplitScreen } from './SplitScreen';
+import { LargePersonListItem } from './people/LargePersonListItem';
+import { SmallPersonListItem } from './people/SmallPersonListItem';
+import { RegularList } from './RegularList';
+// import { SplitScreen } from './SplitScreen';
 
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: 'green'}}>{name}</h1>
-} 
+const people = [{
+  name: 'John Doe',
+  age: 54,
+  hairColor: 'brown',
+  hobbies: ['swimming', 'bicycling', 'video games']
+},{
+  name: 'Brenda Smith',
+  age: 33,
+  hairColor: 'black',
+  hobbies: ['golf', 'math']
+},{
+  name: 'Jane Garcia',
+  age: 27,
+  hairColor: 'blonde',
+  hobbies: ['biology', 'medicine', 'gymnastics']
+}]
 
-const RightHandComponent = ({ message }) => {
-  return <h1 style={{ backgroundColor: 'red'}}>{message}</h1>
-} 
+const products = [{
+  name: 'Flat-Screen TV',
+  price: '$300',
+  description: 'Huge LCD screen, great deal!',
+  rating: 4.5
+}, {
+  name: 'Basketball',
+  price: '$10',
+  description: 'Just like the pros use',
+  rating: 3.8
+}, {
+  name: 'Running Shoes',
+  price: '$120',
+  description: 'State-of-the-art technology for optimum running',
+  rating: 4.2
+}]
 
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}
-    >
-      <LeftHandComponent name={"Steve"}/>
-      <RightHandComponent message={"Hello!"}/>
-    </SplitScreen>
+    <>
+      <RegularList 
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <RegularList 
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+    </>
   );
 }
 
