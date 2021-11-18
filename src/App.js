8217c6ab65f9cfad156b4ac919c81baa980af41e
Steven Ross <ross.steven@me.com>
2021-11-18 16:52:07 -0500

    express routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
-import { NumberedList } from './NumberedList';
-import './App.css';
-import { LargePersonListItem } from './people/LargePersonListItem';
-import { SmallPersonListItem } from './people/SmallPersonListItem';
-import { LargeProductListItem } from './products/LargeProductListItem';
-import { RegularList } from './RegularList';
-import { Modal } from './Modal';
-// import { SplitScreen } from './SplitScreen';
-
-const people = [{
-  name: 'John Doe',
-  age: 54,
-  hairColor: 'brown',
-  hobbies: ['swimming', 'bicycling', 'video games']
-},{
-  name: 'Brenda Smith',
-  age: 33,
-  hairColor: 'black',
-  hobbies: ['golf', 'math']
-},{
-  name: 'Jane Garcia',
-  age: 27,
-  hairColor: 'blonde',
-  hobbies: ['biology', 'medicine', 'gymnastics']
-}]
-
-const products = [{
-  name: 'Flat-Screen TV',
-  price: '$300',
-  description: 'Huge LCD screen, great deal!',
-  rating: 4.5
-}, {
-  name: 'Basketball',
-  price: '$10',
-  description: 'Just like the pros use',
-  rating: 3.8
-}, {
-  name: 'Running Shoes',
-  price: '$120',
-  description: 'State-of-the-art technology for optimum running',
-  rating: 4.2
-}]
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { UserInfo } from "./UserInfo";
 
 function App() {
   return (
     <>
-      <Modal>
-        <LargeProductListItem product={products[0]}/>
-      </Modal>
+      <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader>
     </>
   );
 }
