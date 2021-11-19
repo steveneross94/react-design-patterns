ceee1ea8fdfcf44d4b896cc601beaee362093d23
Steven Ross <ross.steven@me.com>
2021-11-19 11:14:58 -0500

    user ids

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
 function App() {
   return (
     <>
-      <CurrentUserLoader>
+      <UserLoader userId="234">
         <UserInfo />
-      </CurrentUserLoader>
+      </UserLoader>
+      <UserLoader userId="123">
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId="345">
+        <UserInfo />
+      </UserLoader>
     </>
   );
 }
