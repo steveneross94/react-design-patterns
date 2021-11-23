8ad4a3b9445f0c2e741395d407075824827ef2cf
Steven Ross <ross.steven@me.com>
2021-11-23 14:23:45 -0500

    HOC finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { UserInfo } from "./UserInfo";
-import { withUser } from "./withUser";
+import { UserInfoForm } from "./UserInfoForm";
 
-const UserInfoWithLoader = withUser(UserInfo, '234');
 
 function App() {
 
   return (
-    <UserInfoWithLoader />
+    <UserInfoForm />
   );
 }
 
---
+++ src/UserInfoForm.js
@@ -0,0 +1,18 @@
+import { withEditableResource } from "./withEditableResource";
+
+export const UserInfoForm = withEditableResource(({ user, onChangeUser, onSaveUser, onResetUser }) => {
+  const { name, age, hairColor } = user || {};
+
+  return user ? (
+      <>
+        <label>Name: </label>
+        <input value={name} onChange={e => onChangeUser({ name: e.target.value})} />
+        <label>Age: </label>
+        <input type="number" value={age} onChange={e => onChangeUser({ age: Number(e.target.value)})} />
+        <label>Hair Color: </label>
+        <input value={hairColor} onChange={e => onChangeUser({ hairColor: e.target.value})} />
+        <button onClick={onResetUser}>Reset</button>
+        <button onClick={onSaveUser}>Save</button>
+      </>
+  ) : <p>Loading...</p>
+}, '/users/123', 'user')
